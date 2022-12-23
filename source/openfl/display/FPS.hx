39774b3ace0401680cca0b849ca28cde64b257ee
fnfpwwwwwp <117086223+fnfpwwwwwp@users.noreply.github.com>
2022-12-23 15:58:40 +0800

    lol

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -46,7 +46,7 @@ class FPS extends TextField
 		currentFPS = 0;
 		selectable = false;
 		mouseEnabled = false;
-		defaultTextFormat = new TextFormat((Paths.font("vcr.ttf"), color)
+		defaultTextFormat = new TextFormat(Paths.font("vcr.ttf"), color)
 		multiline = true;
 		text = "FPS: ";
 
